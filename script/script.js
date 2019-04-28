8c0d3f939f929028b525d6783a5aae63e96ef073
Alvin <alvinsim74@gmail.com>
2019-04-28 21:51:12 +0800

    added javascript for info passing to enquiry

---
+++ script/script.js
@@ -0,0 +1,21 @@
+function storeShoe(shoename){
+    sessionStorage.shoename = shoename;
+
+}
+
+function validateClick(){
+    var shoename = document.getElementsByTagName("h2")[0].innerHTML;
+    storeShoe(shoename);
+}
+
+function getShoe(){
+    if(sessionStorage.shoename !=undefined){
+        document.getElementById("subject").value = "RE:Enquiry on "+ sessionStorage.shoename;
+    }
+}
+
+function init(){
+    getShoe()
+}
+
+window.onload = init;
