6352497496805565ddf5cf3d09a638b622f7c4aa
joeytzy <44018390+joeytzy@users.noreply.github.com>
2019-05-23 20:30:34 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,37 @@
+        <footer>
+                <p>Email: <a href="mailto:101214747@students.swinburne.edu.my">101214747@students.swinburne.edu.my</a></p>
+            <div class="footer_list">
+                <ul>
+                    <li><a href="abouthypecenter.html">About HypeCenter</a></li>
+                    <li><a href="enquiry.php">Enquire Products</a></li>
+                    <li><a href="enhancements.html">Enhancements</a></li>
+                </ul>
+            </div>
+            <div class="footer_list">
+                    <ul>
+                        <li><a href="enhancements2.html">Assignment 2 Enhancements</a></li>
+                    </ul>
+            </div>
+    
+            <div id="brand-logo-footer">
+                <a href="adidas.html"><img src="images/adidas.png" alt="adidas logo"/></a>
+                <a href="nike.html"><img src="images/nikelogo.png" alt="nike logo"/></a>
+                <a href="converse.html"><img src="images/converse.png" alt="converse logo"/></a>
+                <a href="vans.html"><img src="images/vanslogo.jpg" alt="vans logo"/></a>
+            </div>
+    
+            <div id="team-members">
+            <h3>Team Profiles</h3>
+                <ul>
+                    <li><a href="aboutalvin.html">Alvin</a></li>
+                    <li><a href="aboutdesmond.html">Desmond</a></li>
+                    <li><a href="aboutjoey.html">Joey</a></li>
+                    <li><a href="abouthans.html">Hans</a></li>
+                </ul>
+            </div>
+                    
+            <div id="copyright">
+                <p>&#169;2019 Kiwano | <a href="disclaimer.html">Disclaimer</a> | Last Updated: 8/5/2019</p>
+            </div>
+            
+        </footer>
